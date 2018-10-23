e5013798ec7874d74db26fa4637c0e4ac0655296
Misty <misty@mistys-mbp.wireless.newschool.edu>
2018-10-23 10:10:27 -0400

    javascripttt

--- js/01_intro.js
+++ js/01_intro.js
@@ -2,3 +2,19 @@ var addOne = function(number) {
   return number + 1;
 };
 
+
+/*
+	
+var hsl = "hsl(" + hue + ", 100%, 100%)";
+
+Generate an hsl color with hue 
+as an input in this format:
+"hsl(20, 100%, 100%)"
+
+*/
+
+var hslColor = function(hue) {
+	var color = "hsl(" + hue + ", 100%, 100%)";
+	return color;
+};
+
